26bd3c00094fb48ecafeb6eb843ade13303c3be1
saleempirwani <salimraza3465@gmail.com>
2022-07-12 18:38:50 +0500

    updated

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+// import Home from './pages/Home';
+// import Project from './pages/Project';
+// import NotFound from './pages/NotFound';
+
+
+
+function App() {
+  return (
+    <>
+          <Header />
+    </>
+  );
+}
+
+export default App;
